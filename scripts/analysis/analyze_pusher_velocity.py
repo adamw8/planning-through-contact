c072004d69d330213f6ba623c4cc15c191fa6029
Adam Wei <weiadam@mit.edu>
2025-01-02 15:17:02 -0500

    Improve velocity analysis script and report success rates from launch eval script

--- scripts/analysis/analyze_pusher_velocity.py
+++ scripts/analysis/analyze_pusher_velocity.py
@@ -1,18 +1,20 @@
 import argparse
 import os
+import pickle
 
 import matplotlib.pyplot as plt
 import numpy as np
 import zarr
 
 
-def main(dataset_path):
+def analyze_dataset(dataset_path):
     root = zarr.open(dataset_path)
     pusher_state = np.array(root["data/state"])
     episode_ends = np.array(root["meta/episode_ends"])
 
     pusher_speeds = []
     episode_start = 0
+    DT = 0.1
     for i, episode_end in enumerate(episode_ends):
         pusher_traj = pusher_state[episode_start:episode_end]
 
@@ -23,7 +25,44 @@ def main(dataset_path):
         pusher_traj = pusher_traj[start_idx:]
 
         # Compute average pusher speed
-        pusher_speed = np.linalg.norm(np.diff(pusher_traj, axis=0), axis=1).mean()
+        pusher_speed = np.linalg.norm(np.diff(pusher_traj, axis=0), axis=1).mean() / DT
+        pusher_speeds.append(pusher_speed)
+
+        if i > 500:
+            break
+
+    print(f"Average pusher speed: {np.mean(pusher_speeds)}")
+    print(f"Standard deviation: {np.std(pusher_speeds)}")
+
+    # Plot histogram of pusher speeds
+    plt.hist(pusher_speeds, bins=50)
+    plt.xlabel("Average Pusher Speed")
+    plt.ylabel("Frequency")
+    plt.title("Histogram of Pusher Speeds")
+    plt.show()
+
+
+def analyze_eval(combined_pkl_dir):
+    pusher_speeds = []
+    for i, combined_pkl_file in enumerate(os.listdir(combined_pkl_dir)):
+        if not combined_pkl_file.endswith(".pkl"):
+            continue
+        combined_pkl_path = os.path.join(combined_pkl_dir, combined_pkl_file)
+        with open(combined_pkl_path, "rb") as f:
+            data = pickle.load(f)
+        pusher_data = data.pusher_actual
+        t = pusher_data.t
+        DT = t[1] - t[0]
+        pusher_traj = np.column_stack((pusher_data.x, pusher_data.y))
+
+        # Determine the starting index for significant movement
+        start_idx = detect_movement_start(pusher_traj, threshold=0.001)
+        if start_idx < 0:
+            continue
+        pusher_traj = pusher_traj[start_idx:]
+
+        # Compute average pusher speed
+        pusher_speed = np.linalg.norm(np.diff(pusher_traj, axis=0), axis=1).mean() / DT
         pusher_speeds.append(pusher_speed)
 
         if i > 500:
@@ -60,4 +99,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("dataset_path", type=str, help="Path to the dataset file.")
     args = parser.parse_args()
-    main(args.dataset_path)
+    if args.dataset_path.endswith(".zarr"):
+        analyze_dataset(args.dataset_path)
+    else:
+        analyze_eval(args.dataset_path)
--- scripts/planar_pushing/launch_eval.py
+++ scripts/planar_pushing/launch_eval.py
@@ -1,6 +1,7 @@
 import argparse
 import csv
 import os
+import pickle
 import shutil
 import subprocess
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -14,6 +15,7 @@ BASE_COMMAND = [
     f"--config-dir={CONFIG_DIR}",
     f"--config-name={CONFIG_NAME}",
 ]
+SUCCESS_RATES = {}
 
 # ---------------------------------------------------------
 # Example Usage:
@@ -95,11 +97,21 @@ def run_simulation(checkpoint_path, run_dir):
 
     if result.returncode == 0:
         print(f"\n✅ Completed: {run_dir}")
+
+        # Compute success rate
+        summary_file = os.path.join(run_dir, "summary.pkl")
+        with open(summary_file, "rb") as f:
+            summary = pickle.load(f)
+        success_rate = len(summary["successful_trials"]) / len(summary["trial_times"])
     else:
         print(f"\n❌ Failed: {run_dir}\nError: {result.stderr}")
+        success_rate = None
 
+    global SUCCESS_RATES
+    SUCCESS_RATES[run_dir] = success_rate
     print("\n" + "=" * 50)
     print(f"=== JOB END: {run_dir} ===")
+    print(f"Success Rate: {success_rate}")
     print("=" * 50 + "\n")
 
 
@@ -145,6 +157,13 @@ def main():
                 print(
                     f"❌ Error in job with checkpoint: {checkpoint}, run_dir: {run_dir}\n{e}"
                 )
+
+    # print success rates
+    print("\n" + "=" * 50)
+    print("=== SUCCESS RATES ===")
+    for run_dir, success_rate in SUCCESS_RATES.items():
+        print(f"{run_dir}: {success_rate}")
+    print("=" * 50 + "\n")
 
 
 if __name__ == "__main__":
