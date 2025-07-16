e205f596c9cce86f4fb4535102a763987845e2be
Adam Wei <weiadam@mit.edu>
2025-07-16 12:12:13 -0400

    Rename script

--- scripts/planar_pushing/submit_supercloud_eval.sh
+++ scripts/planar_pushing/submit_supercloud_eval.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Usage
-# LLsub ./submit_checkpoint_eval.sh -s 20 -g volta:1 # TODO: potentially just use cpu
+# LLsub sripts/planar_pushing/submit_supercloud_eval.sh -s 20 -g volta:1 # TODO: potentially just use cpu
 
 # Initialize and Load Modules
-echo "[submit_checkpoint_eval.sh] Loading modules and virtual environment"
+echo "[submit_supercloud_eval.sh] Loading modules and virtual environment"
 source /etc/profile
 module load anaconda/2023a
 export PYTHONPATH=~/workspace/ambient-diffusion-policy:$PYTHONPATH
 echo $PYTHONPATH
 
 # Activate virtual environment
-echo "[submit_checkpoint_eval.sh] Activating virtual environment"
+echo "[submit_supercloud_eval.sh] Activating virtual environment"
 cd /home/gridsan/awei/workspace/planning-through-contact
 source activate /home/gridsan/awei/.conda/envs/planning_through_contact
 
 # Set wandb to offline since Supercloud has no internet access
-echo "[submit_checkpoint_eval.sh] Setting wandb to offline"
+echo "[submit_supercloud_eval.sh] Setting wandb to offline"
 wandb offline
 
 # Export date, time, environment variables
@@ -24,15 +24,15 @@ DATE=`date +"%Y.%m.%d"`
 TIME=`date +"%H.%M.%S"`
 HYDRA_FULL_ERROR=1
 
-echo "[submit_checkpoint_eval.sh] Running eval code..."
-echo "[submit_checkpoint_eval.sh] Date: $DATE"
-echo "[submit_checkpoint_eval.sh] Time: $TIME"
+echo "[submit_supercloud_eval.sh] Running eval code..."
+echo "[submit_supercloud_eval.sh] Date: $DATE"
+echo "[submit_supercloud_eval.sh] Time: $TIME"
 
 # Record start time
 START_TIME=$(date +%s)
 
 # Create temporary config file
-echo "[submit_checkpoint_eval.sh] Creating temporary config file"
+echo "[submit_supercloud_eval.sh] Creating temporary config file"
 
 CONFIG_FILE="config/launch_eval_tmp.txt"
 cat <<EOF > "$CONFIG_FILE"
@@ -40,7 +40,7 @@ checkpoint_path,run_dir,config_name
 $HOME/workspace/ambient-diffusion-policy/data/outputs/ambient_diffusion/planar_pushing/ambient_loss/50_2000_t_min_05_sample/checkpoints/latest.ckpt, eval/ambient_diffusion/planar_pushing/test_sc, gamepad_teleop_carbon.yaml
 EOF
 
-echo "[submit_checkpoint_eval.sh] Running eval command..."
+echo "[submit_supercloud_eval.sh] Running eval command..."
 python scripts/planar_pushing/launch_eval.py --csv-path "$CONFIG_FILE" --max-concurrent-jobs 8 --num-trials 50 50 100
 
 # Remove temporary config file
@@ -53,4 +53,4 @@ HOURS=$((DURATION / 3600))
 MINUTES=$(( (DURATION % 3600) / 60 ))
 SECONDS=$((DURATION % 60))
 
-echo "[submit_checkpoint_eval.sh] Job completed in ${HOURS}h ${MINUTES}m ${SECONDS}s"
+echo "[submit_supercloud_eval.sh] Job completed in ${HOURS}h ${MINUTES}m ${SECONDS}s"
