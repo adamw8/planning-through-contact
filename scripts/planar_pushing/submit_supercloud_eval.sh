ae98dc12197439cfe1df23eea2eb865848b98362
Adam Wei <weiadam@mit.edu>
2025-07-16 13:15:54 -0400

    Testing new changeS

--- scripts/planar_pushing/submit_supercloud_eval.sh
+++ scripts/planar_pushing/submit_supercloud_eval.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Usage
-# LLsub sripts/planar_pushing/submit_supercloud_eval.sh -s 20 -g volta:1 # TODO: potentially just use cpu
+# Usage (cpu nodes)
+# LLsub scripts/planar_pushing/submit_supercloud_eval.sh -s 24
 
 # Initialize and Load Modules
 echo "[submit_supercloud_eval.sh] Loading modules and virtual environment"
@@ -14,10 +14,6 @@ echo $PYTHONPATH
 echo "[submit_supercloud_eval.sh] Activating virtual environment"
 cd /home/gridsan/awei/workspace/planning-through-contact
 source activate /home/gridsan/awei/.conda/envs/planning_through_contact
-
-# Set wandb to offline since Supercloud has no internet access
-echo "[submit_supercloud_eval.sh] Setting wandb to offline"
-wandb offline
 
 # Export date, time, environment variables
 DATE=`date +"%Y.%m.%d"`
@@ -32,16 +28,22 @@ echo "[submit_supercloud_eval.sh] Time: $TIME"
 START_TIME=$(date +%s)
 
 # Create temporary config file
+# TODO: potentially do this step outside of this script
 echo "[submit_supercloud_eval.sh] Creating temporary config file"
 
 CONFIG_FILE="config/launch_eval_tmp.txt"
+CHECKPOINT_PATH="~/workspace/ambient-diffusion-policy/data/outputs/ambient_diffusion/planar_pushing/denoising_loss/sigma_min/50_2000_t_min_04_epsilon/checkpoints/epoch\=0030-val_loss_0\=0.0269-val_ddim_mse_0\=0.0002.ckpt"
+RUN_DIR="eval/ambient_diffusion/planar_pushing/test_sc"
+CONFIG_NAME="gamepad_teleop_carbon.yaml"
+
+rm "$CONFIG_FILE"
 cat <<EOF > "$CONFIG_FILE"
 checkpoint_path,run_dir,config_name
-$HOME/workspace/ambient-diffusion-policy/data/outputs/ambient_diffusion/planar_pushing/ambient_loss/50_2000_t_min_05_sample/checkpoints/latest.ckpt, eval/ambient_diffusion/planar_pushing/test_sc, gamepad_teleop_carbon.yaml
+$CHECKPOINT_PATH, $RUN_DIR, $CONFIG_NAME
 EOF
 
 echo "[submit_supercloud_eval.sh] Running eval command..."
-python scripts/planar_pushing/launch_eval.py --csv-path "$CONFIG_FILE" --max-concurrent-jobs 8 --num-trials 50 50 100
+python scripts/planar_pushing/launch_eval.py --csv-path "$CONFIG_FILE" --max-concurrent-jobs 1 --num-trials 50 50 100
 
 # Remove temporary config file
 rm "$CONFIG_FILE"
